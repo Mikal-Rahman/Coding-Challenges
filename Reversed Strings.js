59139d5f3a1bd5c0c40f85f15bd8720ef5eb3290
Mikal-Rahman <ibnmikal@gmail.com>
2023-03-18 11:23:08 +0700

    Reversed Strings

---
+++ Reversed Strings.js
@@ -0,0 +1,15 @@
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+const solution = str => str.split("").reverse().join("");
+
+
+//--------------------or--------------------
+const solution = str => str.split('').reverse().join('');
+
+//--------------------or--------------------
+function solution(str){
+  return str.split('').reverse().join('');
+}
