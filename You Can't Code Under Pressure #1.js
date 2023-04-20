60972fa61d680720697160bc57e4c7e00fe3b0f0
Mikal-Rahman <ibnmikal@gmail.com>
2023-04-20 08:50:46 +0700

    You Can't Code Under Pressure #1

---
+++ You Can't Code Under Pressure #1.js
@@ -0,0 +1,11 @@
+// Code as fast as you can! You need to double the integer and return it.
+
+
+function doubleInteger(i) {
+    let sum = i + i
+    return sum;
+  }
+
+//------------------or------------------
+
+const doubleInteger = i => i*2; 
