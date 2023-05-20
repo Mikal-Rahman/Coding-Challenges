a7271313f6a5083227bfd534ccaf4ff412109d99
Mikal-Rahman <ibnmikal@gmail.com>
2023-05-20 16:59:45 +0700

    Interactive Dictionary

---
+++ Interactive Dictionary.js
@@ -0,0 +1,30 @@
+// In this kata, your job is to create a class Dictionary which you can add words to and their entries. Example:
+
+// >>> let d = new Dictionary();
+
+// >>> d.newEntry("Apple", "A fruit that grows on trees");
+
+// >>> console.log(d.look("Apple"));
+// A fruit that grows on trees
+
+// >>> console.log(d.look("Banana"));
+// Can't find entry for Banana
+// Good luck and happy coding!
+
+class Dictionary {
+  constructor() {
+    this.entries = {};
+  }
+
+  newEntry(key, value) {
+    this.entries[key] = value;
+  }
+
+  look(key) {
+    if (this.entries.hasOwnProperty(key)) {
+      return this.entries[key];
+    } else {
+      return "Can't find entry for " + key;
+    }
+  }
+}
